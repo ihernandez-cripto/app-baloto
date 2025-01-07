6ebd9ddfbb5732d63db8d5a857cd7c835f78c27e
Ivan Hernandez <134563888+ihernandez-cripto@users.noreply.github.com>
2025-01-07 17:10:04 -0500

    update bloto.js

--- baloto.js
+++ baloto.js
@@ -4,9 +4,10 @@ const colorLoto = ['1BL', '2BL', '3BL', '4BL', '5BL', '6BL', '3AM', '5AM', '6AM'
 const baLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42','43'];
 const miLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39'];
 const superBalota = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
+
 // Arreglo para almacenar los resultados
 let resultados = [];
-let arregloInicializado = [];
+let arregloCompleto = [];
 
 // Función para seleccionar y eliminar un elemento aleatorio
 function seleccionarAleatorio(array) {
@@ -16,22 +17,22 @@ function seleccionarAleatorio(array) {
   return elementoSeleccionado;
 }
 
-// Función para inicializar los elementos del arreglo aleatoriamente
-function inicializarElementos(array) {
-  for (let i = 0; i < array.length; i++) {
-    const elementoAleatorio = seleccionarAleatorio(array);
-    arregloInicializado.push(elementoAleatorio);
+// Esta funcion baraja los elementos del arreglo, siempre estara ordenado de forma diferente
+function barajarArreglo(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Intercambia los elementos
   }
-  return arregloInicializado;
+  return array;
 }
 
-// Funcion para seleccionar aleatoriamente un elemento del arreglo inicializado 
-// y construir el arreglo de la puesta
 function mezclar(nombreArreglo, numeroApostar){
+    // Barajar el arreglo antes de seleccionar los números
+    const arregloBarajado = barajarArreglo(nombreArreglo.slice());
     resultados = [];
-    let arregloUtilizado = nombreArreglo.slice(); // crear el respaldo del arreglo original
+    arregloCompleto = arregloBarajado; // crear el respaldo del arreglo original
     for (let i = 0; i < numeroApostar; i++) {
-        const elementoSeleccionado = seleccionarAleatorio(inicializarElementos(arregloUtilizado));
+        const elementoSeleccionado = seleccionarAleatorio(eval(arregloCompleto));
         resultados.push(elementoSeleccionado);
       }
       document.querySelector('.input-1').value=resultados;
