da4e0117c7eae3d8769c9605802477a933e360b7
Ivan Hernandez <134563888+ihernandez-cripto@users.noreply.github.com>
2025-01-14 12:48:47 -0500

    WOLSBURG

--- baloto.js
+++ baloto.js
@@ -1,5 +1,5 @@
 // Arreglo original
-const colorLoto = ['LRJ', 'LRJ','LRJ', 'LVD','VVD','VVD','VVD', 'VRJ', 'E', 'E', 'E']; 
+const colorLoto = ['LRJ', 'LRJ', 'LVD','VVD','VVD', 'VRJ', 'E', 'E', 'E']; 
 // const colorLoto = ['2BL', '3BL', '4BL', '5BL', '6BL', '2AM', '3AM', '5AM', '6AM', '7AM', '2NG', '3NG', '5NG', '6NG', '1RJ', '2RJ', '3RJ', '4RJ', '5RJ', '7RJ', '1VD', '2VD', '3VD', '5VD', '6VD', '7VD', '3AZ', '4AZ', '5AZ', '6AZ', '7AZ'];
 // const colorLoto = ['1BL', '2BL', '3BL', '4BL', '5BL', '6BL', '7BL', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '1NG', '2NG', '3NG', '4NG', '5NG', '6NG', '7NG', '1RJ', '2RJ', '3RJ', '4RJ', '5RJ', '6RJ', '7RJ', '1VD', '2VD', '3VD', '4VD', '5VD', '6VD', '7VD', '1AZ', '2AZ', '3AZ', '4AZ', '5AZ', '6AZ', '7AZ'];
 const baLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42','43'];
